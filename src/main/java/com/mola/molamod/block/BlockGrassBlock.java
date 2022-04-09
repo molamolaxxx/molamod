f505f46a58315d44af1433f7c0596276c0baa525
molamolaxxx <408287749@qq.com>
2022-04-10 02:31:24 +0800

    fix

--- src/main/java/com/mola/molamod/block/BlockGrassBlock.java
+++ src/main/java/com/mola/molamod/block/BlockGrassBlock.java
@@ -9,8 +9,18 @@ import com.mola.molamod.utils.ModelLoaderUtil;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import org.apache.logging.log4j.Logger;
+import scala.actors.threadpool.ExecutorService;
+import scala.actors.threadpool.Executors;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author : molamola
@@ -25,6 +35,8 @@ public class BlockGrassBlock extends Block implements IModelRender {
 
     private Logger logger = LoggerUtil.getLogger();
 
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
+
     public BlockGrassBlock() {
         super(Material.GROUND);
         this.setUnlocalizedName("mola." + name);
@@ -35,11 +47,74 @@ public class BlockGrassBlock extends Block implements IModelRender {
         this.setCreativeTab(MolaMod.MOLA_TAB);
     }
 
+    @Override
+    public void onBlockClicked(World worldIn, BlockPos pos, EntityPlayer playerIn) {
+        if (worldIn.isRemote) {
+            UpdatePositionTask updatePositionTask = new UpdatePositionTask(worldIn, pos, 2, 2);
+//            executorService.submit(updatePositionTask);
+            updatePositionTask.run();
+        }
+    }
 
     @Override
     public void onItemRender() {
         logger.info("[item] 草方块开始渲染");
         ItemBlock itemBlock = CustomHandlerManager.getItemHandler().getItemBlock(getRegistryName());
         ModelLoaderUtil.setCustomModelResourceLocation(itemBlock, 0, "inventory");
+    }
+
+    public class UpdatePositionTask implements Runnable {
+
+        private World worldIn;
+        private BlockPos currentPos;
+        private int paddingX;
+        private int paddingZ;
+
+        public UpdatePositionTask(World worldIn, BlockPos startPos, int paddingX, int paddingZ) {
+            this.worldIn = worldIn;
+            this.currentPos = startPos;
+            this.paddingX = paddingX;
+            this.paddingZ = paddingZ;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 1; i++) {
+                List<BlockPos> allNeedMoveBlockPosition = getAllNeedMoveBlockPosition();
+                for (BlockPos blockPos : allNeedMoveBlockPosition) {
+                    IBlockState blockState = worldIn.getBlockState(blockPos);
+                    worldIn.setBlockState(blockPos.add(0, 1, 0), blockState, 2);
+                    worldIn.setBlockState(blockPos, Blocks.AIR.getDefaultState(), 2);
+                }
+
+                this.currentPos = currentPos.add(0, 1, 0);
+
+//                waitOneSecond();
+
+                logger.info("UpdatePositionTask run");
+            }
+        }
+
+        /**
+         * 获取所有需要被移动的方块
+         * @return
+         */
+        private List<BlockPos> getAllNeedMoveBlockPosition() {
+            List<BlockPos> blockPosList = new ArrayList<>(paddingX * 2 + paddingZ * 2 + 4);
+            for (int i = -paddingX; i <= paddingX; i++) {
+                for (int j = -paddingZ; j <= paddingZ; j++) {
+                    BlockPos blockPos = currentPos.add(i, 0, j);
+                    blockPosList.add(blockPos);
+                }
+            }
+            return blockPosList;
+        }
+
+        private void waitOneSecond() {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
+        }
     }
 }
